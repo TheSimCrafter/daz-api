87d1e76b49434acdc96d2652e248d6caa726194e
Niklas Koch <ni.koch@lvm.de>
2025-05-09 13:31:08 +0200

    Initial

---
+++ src/main/java/de/lebk/dazapi/DazApiApplication.java
@@ -0,0 +1,13 @@
+package de.lebk.dazapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DazApiApplication.class, args);
+    }
+
+}
